d2c152fdebf5c77f2256250e522ac10e8190b972
Dustin Van Tate Testa <toast27@gmail.com>
2016-07-10 21:28:34 -0400

    changes and stuff
    - * -> #
    - more mazes 
    - ending function

--- field.h
+++ field.h
@@ -8,7 +8,7 @@
 #include "utils.h"
 
 #include "controller.h"
-
+#include "maze.h"
 
 extern char maze[9][10];
 extern struct coord9x9 player;
@@ -32,7 +32,7 @@ namespace field {
   void showField(){
     for (uint8_t x = 0; x < 9; x++)
       for (uint8_t y = 0; y < 9; y++)
-        if (maze[x][y] == '*')
+        if (maze[x][y] == '#')
           leds[(x * 9) + y] = CRGB::Red;
         else if (maze[x][y] == ' ')
           leds[(x * 9) + y] = CRGB::Black;
@@ -56,7 +56,7 @@ namespace field {
 
     for (uint8_t x = 0; x < 9; x++)
       for (uint8_t y = 0; y < 9; y++)
-        if (maze[x][y] == 's' || maze[x][y] == 'S') {
+        if (maze[x][y] == 'S') {
           ret.x = x;
           ret.y = y;
           return ret;
@@ -87,27 +87,21 @@ namespace field {
       // this will tamper with r's values making them random 
       randint = random(65535);
       
-      maze[coord.x - 1][coord.y - 1] = r.m00 ? '*' : ' ';
-      maze[coord.x - 1][coord.y] =     r.m01 ? '*' : ' ';
-      maze[coord.x - 1][coord.y + 1] = r.m02 ? '*' : ' ';
+      maze[coord.x - 1][coord.y - 1] = r.m00 ? '#' : ' ';
+      maze[coord.x - 1][coord.y] =     r.m01 ? '#' : ' ';
+      maze[coord.x - 1][coord.y + 1] = r.m02 ? '#' : ' ';
 
-      maze[coord.x][coord.y - 1] = r.m10 ? '*' : ' ';
-      maze[coord.x][coord.y] =     r.m11 ? '*' : ' ';
-      maze[coord.x][coord.y + 1] = r.m12 ? '*' : ' ';
+      maze[coord.x][coord.y - 1] = r.m10 ? '#' : ' ';
+      maze[coord.x][coord.y] =     r.m11 ? '#' : ' ';
+      maze[coord.x][coord.y + 1] = r.m12 ? '#' : ' ';
 
-      maze[coord.x + 1][coord.y - 1] = r.m20 ? '*' : ' ';
-      maze[coord.x + 1][coord.y] =     r.m21 ? '*' : ' ';
-      maze[coord.x + 1][coord.y + 1] = r.m22 ? '*' : ' ';
+      maze[coord.x + 1][coord.y - 1] = r.m20 ? '#' : ' ';
+      maze[coord.x + 1][coord.y] =     r.m21 ? '#' : ' ';
+      maze[coord.x + 1][coord.y + 1] = r.m22 ? '#' : ' ';
     }
   }
 
 
-  // randomly generate the maze
-  void makeMaze(){
-
-
-
-  }
 
   void updatePlayer(){
     
@@ -115,26 +109,43 @@ namespace field {
     struct inputDir input = controls::dirInput();
 
     // if it's safe to move then move
-    if (input.up && player.x != 0 && maze[player.x - 1][player.y] != '*')
+    if (input.up && player.x != 0 && maze[player.x - 1][player.y] != '#')
       player.x--;
-    if (input.down && player.x != 8 && maze[player.x + 1][player.y] != '*')
+    if (input.down && player.x != 8 && maze[player.x + 1][player.y] != '#')
       player.x++;
-    if (input.left && player.y != 0 && maze[player.x][player.y - 1] != '*')
+    if (input.left && player.y != 0 && maze[player.x][player.y - 1] != '#')
       player.y--;
-    if (input.left && player.y != 8 && maze[player.x][player.y + 1] != '*')
+    if (input.left && player.y != 8 && maze[player.x][player.y + 1] != '#')
       player.y++;
       
   }
 
-  void clearScreen(){
+
+  inline bool gameOver()
+    { return maze[player.x][player.y] == 'E'; }
+
+  inline void clearScreen(){
     for (uint8_t i = 0; i < NUM_LEDS; i++)
       leds[i] = CRGB::Black;
+  }
+
+  // reset the game board and have some fun :D
+  void endGame(){
+    
+    // clearScreen();
+
+    // display GG 
+    Maze::setMaze(Maze::gg);
+    showField();
+    delay(5000);
+
+    // draw in more players
+    Maze::setMaze(Maze::hello);
+    showField();
     
   }
 
-  
-
-}
+} 
 
 
 #endif
