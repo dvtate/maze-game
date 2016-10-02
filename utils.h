bcb21a35c99ecf446feef369f77e6c26f3fece3b
Dustin Van Tate Testa <toast27@gmail.com>
2016-10-02 01:05:53 -0400

    add some more POD types :)))

--- utils.h
+++ utils.h
@@ -8,4 +8,23 @@ struct coord9x9 {
 
 };
 
+// used for storing D-pad input
+struct inputDir {
+  bool up    : 1,
+       down  : 1,
+       left  : 1,
+       right : 1;
+};
+
+// used for storing sub-grid values
+struct subgrid_t {
+  // matrix is more programmer-friendly
+  // bit-field is more memory-optimized
+  // save memory, save lives ;)
+  bool m00 : 1, m01 : 1, m02 : 1,
+       m10 : 1, m11 : 1, m12 : 1,
+       m20 : 1, m21 : 1, m22 : 1;
+};
+
+
 #endif
