4444781eb64f40ecd68aa812d72ebf47ea7fb21f
Dustin Van Tate Testa <toast27@gmail.com>
2017-02-22 01:09:51 -0800

    fix indentation issues

--- enemy.h
+++ enemy.h
@@ -1,104 +1,97 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
-
 #include "utils.h"
 #include "controller.h"
 #include "maze.h"
 
-
 extern char maze[9][10];
 extern struct coord9x9 player;
 
+// he wants to eat you
 struct coord9x9 enemy;
 
 
 namespace Enemy {
 
   // returns the coordinates of the enemy spawn-point
-	static struct coord9x9 findEnemySpawn(){
-		struct coord9x9 ret;
-    
-		for (uint8_t r = 0; r < 9; r++)
-			for (uint8_t c = 0; c < 9; c++)
-				if (maze[r][c] == 'X') {
-					ret.r = r;
-					ret.c = c;
-					return ret;
-				}
+  static struct coord9x9 findEnemySpawn(){
+    struct coord9x9 ret;
 
-		ret.r = 13;
-		return ret;
-	}
+    for (uint8_t r = 0; r < 9; r++)
+      for (uint8_t c = 0; c < 9; c++)
+        if (maze[r][c] == 'X') {
+          ret.r = r;
+          ret.c = c;
+          return ret;
+        }
+
+    ret.r = 13;
+    return ret;
+  }
 
 
-	inline void goToSpawn()
-		{ enemy = findEnemySpawn(); }
+  inline void goToSpawn()
+    { enemy = findEnemySpawn(); }
 
   // changes the enemy coords if its within the game boundaries
-	static void moveEnemy(struct inputDir movement){
+static void moveEnemy(struct inputDir movement){
+  // if it's safe to move then move...
+  if (movement.up && enemy.r != 0 && maze[enemy.r - 1][enemy.c] != '#')
+   enemy.r--;
+  if (movement.down && enemy.r != 8 && maze[enemy.r + 1][enemy.c] != '#')
+    enemy.r++;
+  if (movement.left && enemy.c != 0 && maze[enemy.r][enemy.c - 1] != '#')
+    enemy.c--;
+  if (movement.left && enemy.c != 8 && maze[enemy.r][enemy.c + 1] != '#')
+    enemy.c++;
 
-	    // if it's safe to move then move...
-	    
-	    if (movement.up && enemy.r != 0 && maze[enemy.r - 1][enemy.c] != '#')
-	      enemy.r--;
-	      
-	    if (movement.down && enemy.r != 8 && maze[enemy.r + 1][enemy.c] != '#')
-	      enemy.r++;
-	      
-	    if (movement.left && enemy.c != 0 && maze[enemy.r][enemy.c - 1] != '#')
-	      enemy.c--;
-	      
-	    if (movement.left && enemy.c != 8 && maze[enemy.r][enemy.c + 1] != '#')
-	      enemy.c++;
-
-     
-	}
+}
 
   // if there's a block in front of the enemy
-	static bool blockAhead(struct inputDir movement){
-		
-		if (movement.up && (enemy.r == 0 || maze[enemy.r - 1][enemy.c] == '#'))
-			return true;
-		if (movement.down && (enemy.r == 8 || maze[enemy.r + 1][enemy.c] == '#'))
-			return true;
-		if (movement.left && (enemy.c == 0 || maze[enemy.r][enemy.c - 1] == '#'))
-			return true;
-		if (movement.right && (enemy.c == 8 || maze[enemy.r][enemy.c + 1] == '#'))
-			return true;
+static bool blockAhead(struct inputDir movement){
+  if (movement.up && (enemy.r == 0 || maze[enemy.r - 1][enemy.c] == '#'))
+    return true;
+  if (movement.down && (enemy.r == 8 || maze[enemy.r + 1][enemy.c] == '#'))
+    return true;
+  if (movement.left && (enemy.c == 0 || maze[enemy.r][enemy.c - 1] == '#'))
+    return true;
+  if (movement.right && (enemy.c == 8 || maze[enemy.r][enemy.c + 1] == '#'))
+    return true;
 
-		return false;
-			
-	}
+  return false;
+
+}
 
   // returns the direction most opposite to the given direction
-	static struct inputDir& reverseDirection(struct inputDir& movement){
-		
-		if (movement.up) {
-			movement.up = false;
-			movement.down = true;
-		} else if (movement.down) {
-			movement.down = false;
-			movement.up = true;
-		}
+static struct inputDir& reverseDirection(struct inputDir& movement){
 
-		if (movement.left) {
-			movement.left = false;
-			movement.right = true;
-		} else if (movement.right) {
-			movement.right = false;
-			movement.left = true;
-		}
+  if (movement.up) {
+    movement.up = false;
+    movement.down = true;
+  } else if (movement.down) {
+    movement.down = false;
+    movement.up = true;
+  }
 
-		return movement;
-	}
+  if (movement.left) {
+    movement.left = false;
+    movement.right = true;
+  } else if (movement.right) {
+    movement.right = false;
+    movement.left = true;
+  }
+
+  return movement;
+}
 
   // determines if the enemy is is trapped
   // and cannot escape
   bool lockedIn(){
     for (int8_t r = -1; r != 2; r++)
       for (int8_t c = -1; c != 2; c++)
-        if (maze[enemy.r + r][enemy.c + c] != '#' 
+        if (
+          maze[enemy.r + r][enemy.c + c] != '#' 
           && enemy.r != 0 && enemy.r != 8
           && enemy.c != 0 && enemy.c != 8
         )
@@ -158,33 +151,31 @@ namespace Enemy {
 
       coord9x9 pastLoc = enemy;
 
-			// the movement direction is randomized by corrupting it's memory with
-			// a random integer
-			union {
-				unsigned int val : 4;
-				struct inputDir moveDir;
-			};
-
-
-			val = random(16);
+      // the movement direction is randomized by corrupting it's memory with
+      // a random integer
+      union {
+        unsigned int val : 4;
+        struct inputDir moveDir;
+      };
+      val = random(16);
 
       // enemy is most likely to continue to move forward
-			if (val < 10) {
+      if (val < 10) {
 continue_or_reverse:
         // if there is no movement direction, calculate a new one
         if (!movement.up && !movement.down && !movement.left && !movement.right)
           goto random_movement;
           
         // move forward or bounce off wall
-				if (blockAhead(movement))
-					moveEnemy(reverseDirection(movement));
+        if (blockAhead(movement))
+           moveEnemy(reverseDirection(movement));
         else 
           moveEnemy(movement);
         
       // try to turn 90 degrees from current direction
-			} else if (val < 12) {
+      } else if (val < 12) {
         
-			  if (movement.up || movement.down)
+        if (movement.up || movement.down)
           if (enemy.c != 0 && maze[enemy.r][enemy.c - 1] != '#') {
             enemy.c--;
             movement = {0, 0, 1, 0};
@@ -204,23 +195,24 @@ continue_or_reverse:
         // if there is no movement direction, calculate a new one
         else
           goto random_movement;
-			  
-			/// this is supposed to randomly generate a new direction
-		  } else {
+                                
+      /// this is supposed to randomly generate a new direction
+      } else {
 random_movement:        
-  //    moveDir = {0, 0, 0, 0};
-  //    if (!lockedIn())
-  //      while (val == 0 || blockAhead(moveDir))  
-				val = random(16); // try replacing this with random(17)...
+      //moveDir = {0, 0, 0, 0};
+      //if (!lockedIn())
+        //while (val == 0 || blockAhead(moveDir))  
+        
+        val = random(16); // try replacing this with random(17)...
         crunches++; // (;.;)
-				moveEnemy(movement = moveDir);
+        moveEnemy(movement = moveDir);
        
 			}
       
       // if there was no movement, try and generate another direction
       if (enemy.r == pastLoc.r && enemy.c == pastLoc.c && !lockedIn() && crunches < 2000)
         goto random_movement;
-		}
+    }
 		
   }
 
